5da33af62174660351cefa31dc34ee0c95f5759a
Deshna Jain <jain.deshna1309@gmail.com>
2024-07-15 17:50:51 +0530

    added tokens and configured cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,17 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "file"
+import { v2 as cloudinary } from 'cloudinary';
+
+(async function() {
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: '', 
+        api_key: '', 
+        api_secret: '<your_api_secret>' // Click 'View Credentials' below to copy your API secret
+    });
+    
+});
+   
+    
+   
