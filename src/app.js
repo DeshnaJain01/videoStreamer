fa03ae8c334bdad59795acdb57244669b6f408e0
Deshna Jain <jain.deshna1309@gmail.com>
2024-07-19 18:06:10 +0530

    login user method creation

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 //public access of item stores in our public folder
 app.use(express.static("public"))
-//keeping secure cookies on users browser
+//keeping secure cookies on users browser and allow access of cookies
 app.use(cookieParser())
 //routes import 
 import userRouter from './routes/user.routes.js'
